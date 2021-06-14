42504e78b90039c9ca8bd3658d12d9027ce8d7ac
Joseph-Cha <75019048+Joseph-Cha@users.noreply.github.com>
2021-06-14 21:59:40 +0900

    1.11~1.13 수강 완료

--- vscode/MyHeaders/add.h
+++ vscode/MyHeaders/add.h
@@ -1,6 +1,15 @@
 #pragma once
 
+// 전처리기
+// MY_ADD가 이미 정의 되어있으면 MY_ADD를 include하지 마라(정의 하지 마라) 
+// => #pragma once로 한번에 구현 가능
+// #ifndef MY_ADD
+
+// #define My_ADD
+
 int add(int a, int b)
 {
     return a + b;
 }
+
+// #endif
